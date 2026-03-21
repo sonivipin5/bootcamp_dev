fa1c9dc1ee51d8bc1fdabb815da185cad11cdeac
Vipin Soni <sonivipin5@gmail.com>
2026-03-22 00:34:21 +0530

    Node.js API Mastery 3.3  Creating Routes  Responses In Express.mp4

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/api/v1/bootcamper', (req, res) => {
+  res.status(200).json({status:true, message:'Welcome to Bootcamper API'});
+});
+
+app.post('/api/v1/bootcamper', (req, res) => {
+  res.status(200).json({status:true, message:'Bootcamper created successfully'});
+});
+
+app.put('/api/v1/bootcamper/:id', (req, res)=>{
+    res.status(200).json({status:true, message:`Bootcamper updated ${req.params.id} successfully`});
+});
+
+app.delete('/api/v1/bootcamper/:id', (req, res)=>{
+    res.status(200).json({status:true, message:`Bootcamper deleted ${req.params.id} successfully`});
 });
 
 app.listen(process.env.PORT || 5000, () => {

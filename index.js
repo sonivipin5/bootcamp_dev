fd4a7773c50c79711a7f7edadb1fc659168536b5
Vipin Soni <sonivipin5@gmail.com>
2026-03-22 20:11:31 +0530

    Node.js Mastery 3.6. Intro To Middleware.mp4

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import bootcamperRouter from './router/bootcamper.js';
+import { logger } from './middleware/logger.js';
+import morgan from 'morgan';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('combined'))
+}
 
 app.use('/api/v1/bootcamper', bootcamperRouter)
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${process.env.NODE_ENV || 'development'} on port ${process.env.PORT || 5000}`);
 });
